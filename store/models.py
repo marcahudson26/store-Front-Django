a9b90eb2ee95f6b4870642ec268db86464bdade9
marc hudson <marchudson2601@gmail.com>
2023-10-19 23:41:26 +0100

    added database config added mysql client

--- store/models.py
+++ store/models.py
@@ -23,6 +23,8 @@ class Customer(models.Model):
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES,default=MEMBERSHIP_BRONZE)
+    
+
 
 
 class Address(models.Model):
--- storefront/settings.py
+++ storefront/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,8 +91,11 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':env('DB_NAME'),
+        'HOST':env('DB_HOST'),
+        'USER':env('DB_USER'),
+        'PASSWORD':env('DB_PASSWORD')
     }
 }
 
